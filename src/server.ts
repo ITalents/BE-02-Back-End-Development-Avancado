ebaa618260e1c4ce2fb8c5e3836ca70a58b4ad40
codethi <docencia.thiago@gmail.com>
2023-02-20 10:46:30 -0300

    feat: innitial configs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP Server running!");
-  });
+    console.log('HTTP Server running!')
+  })
